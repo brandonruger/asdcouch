7fcd2adee8120ba5eb08c964f3ce7f11626d7adc
r02548 <bruger@fullsail.edu>
2013-11-17 14:33:00 -0500

    Added jquery couch plugin.

--- asdcouch/app/_attachments/main.js
+++ asdcouch/app/_attachments/main.js
@@ -35,22 +35,7 @@ $('#addalbum').on('pageinit', function(){
             localStorage.setItem(generateId, JSON.stringify(itemList));
             alert("Album has been added!");
             
-        //Dynamically create Edit & Delete Links
-    	function createEditDelLinks(key, newListItem) {
-	        var editLink = $('<ul><li><a href="#">Edit Item</a></li></ul>').appendTo("#listoflsdata").on("click", editReminder);
-	        editLink.key = key;
-	        $(editLink).append(newListItem);
-	        console.log(editLink.key);
-	        
-	        //add line break
-	        var breakTag = $('br').appendTo("#addalbumform");
-	        
-	        //add delete single item link
-	        var deleteLink = $('<ul><li><a href="#">Delete Item</a></li></ul>').appendTo("#listoflsdata").on("click", deleteReminder);
-	        deleteLink.key = key;
-	        $(deleteLink).append(newListItem);
-	                
-    	}
+       
     }
     
     	function getDataFromLocalStorage() {
@@ -79,7 +64,22 @@ $('#addalbum').on('pageinit', function(){
         }
     }
 	
-    
+     //Dynamically create Edit & Delete Links
+    	function createEditDelLinks(key, newListItem) {
+	        var editLink = $('<ul><li><a href="#">Edit Item</a></li></ul>').appendTo("#listoflsdata").on("click", editReminder);
+	        editLink.key = key;
+	        $(editLink).append(newListItem);
+	        console.log(editLink.key);
+	        
+	        //add line break
+	        var breakTag = $('br').appendTo("#addalbumform");
+	        
+	        //add delete single item link
+	        var deleteLink = $('<ul><li><a href="#">Delete Item</a></li></ul>').appendTo("#listoflsdata").on("click", deleteReminder);
+	        deleteLink.key = key;
+	        $(deleteLink).append(newListItem);
+	                
+    	}
 
     
      //Auto populate Local Storage with data
@@ -127,6 +127,7 @@ $('#addalbum').on('pageinit', function(){
         var askUser = confirm("Are you sure you want to delete this reminder?");
         if (askUser) {
             localStorage.removeItem(this.key);
+            console.log(this.key);
             alert("Reminder was deleted!");
             window.location.reload();
         }else{
