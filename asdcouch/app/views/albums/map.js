2761018e47afe2ef90e71d1b2edd94c273cc616c
r02548 <bruger@fullsail.edu>
2013-11-20 19:32:25 -0500

    Set up map function to include key and rev values.

--- asdcouch/app/views/albums/map.js
+++ asdcouch/app/views/albums/map.js
@@ -1,6 +1,8 @@
 function(doc) {
   if (doc._id.substr(0,3) === "cd:" || doc._id.substr(0,4) === "mp3:") {
     emit(doc._id, {
+    	"key": doc._id,
+    	"rev": doc._rev,
     	"artist": doc.artist,
     	"album": doc.album,
     	"format": doc.format,
